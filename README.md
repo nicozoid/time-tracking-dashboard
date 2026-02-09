962794171c190b8dd71306f25b81fd4aea02f14a
Nic Zap <17334346+nicozoid@users.noreply.github.com>
2026-02-09 18:27:48 +0000

    initial build

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Time tracking dashboard
 
-WORK IN PROGRESS!
-
 ![Design preview](./preview.jpg)
 
 **[View live solution](https://nicozoid.github.io/time-tracking-dashboard)**
@@ -15,12 +13,25 @@ This is a solution to a [challenge on Frontend Mentor](https://www.frontendmento
 - 
 
 ### Current process
-1. Create HTML, inc. containers & class names, inc. text styles.
-1. Create variables (e.g. color, spacing). 
-1. Text styles: define classes (direct from figma), import fonts.
-1. Full CSS for mobile.
+#### Setup
+1. Set up readme.md
+1. Import fonts
+1. Create JS files and link to it
+1. Create CSS file link to it
+1. Reset browser default values
+1. Add color variables (I'm deliberately ignoring spacing variables for now, to help me practice estimating pixel values)
+
+#### Text classes
+1. Set up defaults in body class
+1. Define text styles in html and css simultaneously, mobile first
+1. Font variations for tablet & desktop
+
+#### Main work
+1. Create HTML tags, classes, and id names, focusing on mobile
+1. Check HTML will work for tablet and desktop
+1. CSS styling for mobile 
 1. Define hover states and transitions
-1. Adjust the CSS as needed for tablet and desktop
+1. CSS styling for tablet and desktop
 
 ### Questions
 I'm always interested in thoughts on whether there is a point in learning any this given the state of AI, or how we should adapt what we focus on in light of AI. I'm learning frontend coding in the belief that AI code still needs human oversight and I can't provide that oversight without coding proficiency. I wonder if even that will be true in a year though.# newsletter-signup-form
---
+++ script.js
@@ -0,0 +1,61 @@
+const btnDaily = document.getElementById('daily-btn');
+const btnWeekly = document.getElementById('weekly-btn');
+const btnMonthly = document.getElementById('monthly-btn');
+
+let timeData;
+
+fetch('/data.json')
+    .then(
+        (response) => {
+            console.log('fetching data');
+            if (!response.ok) return console.log('ERROR');
+            return response.json();
+        }
+    ).then(
+        (database) => {
+            const timeData = database;
+            updateTimes(timeData, 'weekly');
+
+
+            btnDaily.addEventListener('click', () => {
+                updateTimes(timeData, 'daily');
+                updateButtons(btnDaily);
+            });
+
+            btnWeekly.addEventListener('click', () => {
+                updateTimes(timeData, 'weekly');
+                updateButtons(btnWeekly);
+            });
+
+            btnMonthly.addEventListener('click', () => {
+                updateTimes(timeData, 'monthly');
+                updateButtons(btnMonthly);
+            }
+            );
+        }
+
+
+    )
+
+function updateTimes(data, timeframe) {
+    data.forEach(item => {
+        const categoryId = item.title.toLowerCase();
+        let current = item.timeframes[timeframe].current;
+        let previous = item.timeframes[timeframe].previous;
+        if (current === 1) {
+            current = current + 'hr';
+        } else { current = current + 'hrs'; }
+        if (previous === 1) {
+            previous = previous + 'hr';
+        } else { previous = previous + 'hrs'; }
+        document.getElementById(`${categoryId}-current`).textContent = current;
+        document.getElementById(`${categoryId}-previous`).textContent = previous;
+    });
+}
+
+function updateButtons(selected) {
+    btnDaily.classList.remove('active');
+    btnWeekly.classList.remove('active');
+    btnMonthly.classList.remove('active');
+    selected.classList.add('active');
+}
